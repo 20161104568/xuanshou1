dce8ce1d5e9e39c717ae1692a1a9fcceb9657034
20161104568 <a20161104568@admindeimac-9.local>
2018-06-27 09:38:04 +0800

    Initial Commit

---
+++ xuanshou1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  xuanshou1
+//
+//  Created by 20161104568 on 2018/6/27.
+//  Copyright © 2018年 20161104568. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
